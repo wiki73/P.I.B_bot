0f22aeb703743411c62604f001de708db8ac8b41
wiki73 <vladimir.savchenko.an@gmail.com>
2025-03-26 14:27:23 +0300

    попытка перестроить всё в нормальный вид

--- bot.py
+++ bot.py
@@ -1,29 +1,25 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram import F
 import asyncio
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.filters import CommandStart, Command
-from database import create_tables, get_user_name, save_user_name, save_user_plan, get_user_plan
+from database import create_tables, get_user_name, save_user_name, save_user_plan, get_user_plan,get_base_plan,get_plan_name_by_id,update_user_current_plan, get_current_plan, get_db_connection,get_plan_text_by_name
 
-# Замените 'YOUR_BOT_TOKEN' на токен, полученный от @BotFather
 TOKEN = '7947948717:AAF_yeOpDDLoOCrTUWQRzb_akKx05xWpNVU'
-plans = {'Default': 'План - Default:\n Плана date  d.w\n   Подъем:\n1) ......\n2) ......\n3) ......\n4) ......\n5) ......\n   Сон:',
-'Hard': 'План - Hard:\n Плана date  d.w\n   Подъем:\n1) ...... () Время\n2) ......() Время\n3) ......() Время\n4) ......() Время\n5) ......() Время\n   Сон:' }
-
-# Инициализация бота и диспетчера
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
-
-# Создание таблиц пользователей и планов
 create_tables()
 
 
 chech_name = False
-chech_plan = False
+chech_new_plan = False
 chech_create_plan = False
-# Обработчик команды /start
+chech_change_plan = False
+chech_base_plan = False
+
 @dp.message(Command('start'))
 async def start_command(message: types.Message):
+    print("Команда /start")
     global chech_name
     user_id = message.from_user.id
     user_name = get_user_name(user_id)
@@ -31,63 +27,161 @@ async def start_command(message: types.Message):
     if user_name is None:
         await message.reply('Еу. Я твой бот для рассписания, меня завут P.I.B(пиб) - все названия и вся кринжовость текста связанная с моим создателем я думаю ты его знаешь). Я вижу тебя первый раз, так что давай для начала узнаем как мне тебя называть. Мой создатель назвал себя хер и я его теперь пеостоянно так назваю. Не будь как этот конч и давай ка придумай что-нибудь прикольное. ')
         await message.reply("Напиши ник")
-        chech_name = True
-        # Ожидание сообщения с ником
-        
+        chech_name = True # Ожидание сообщения с ником
         
     else:
         await message.reply(f"Еу, {user_name}! Я твой бот для рассписания, меня завут P.I.B(пиб) - все названия и вся кринжовость текста связанная с моим создателем я думаю ты его знаешь). Чем могу помочь?Если что попробуй написать /help")
 
+
+@dp.message(Command('help'))
+async def help_command(message: types.Message):
+    print("Команда /help")
+    await message.reply("Доступные команды:\n"
+                       "/start - Начать работу с ботом\n"
+                       "/help - Показать это сообщение\n "
+                       "/info - Получить информацию о планировании\n"
+                       "/check_current_plan - Посмотреть актуальный план\n"
+                       "/view_base_plans - Посмотреть базовые планы\n"
+                       "/choose_plan - Выбрать или изменить план\n"
+                       "/create_plan - Создать план\n ")
+
+
 @dp.message(Command('create_plan'))
 async def create_plan_command(message: types.Message):
+    print("Команда /create_plan")
     global chech_create_plan
     await message.reply("Пожалуйста, введите текст вашего плана:")
     chech_create_plan = True   # Устанавливаем состояние ожидания плана
 
-# Обработчик команды /view_plans
-@dp.message(Command('view_plans'))
-async def view_plans_command(message: types.Message):
-    user_id = message.from_user.id
-    user_plans = get_user_plan(user_id)  # Получаем план пользователя
-    print(user_plans)
-    if user_plans:
-        for plan in user_plans:
-            await message.reply(f"Ваши план:\n{plan}")
+# Обработчик команды /view_base_plans
+@dp.message(Command('view_base_plans'))
+async def view_base_plans_command(message: types.Message):
+    global chech_base_plan
+    base_plans = get_base_plan() # Получаем план п
+    if base_plans:
+        for plan in base_plans:
+            await message.reply(plan[1])
+            await message.reply(plan[0])
+        await message.reply("Вот планы, если хотите выберите один из них, просто напиши 1, 2, 3 или 4")
+        chech_base_plan = True
     else:
         await message.reply("У вас нет сохраненных планов.")
 
-# Обработчик команды /help
-@dp.message(Command('help'))
-async def help_command(message: types.Message):
-
-    await message.reply("Доступные команды:\n"
-                       "/start - Начать работу с ботом\n"
-                       "/choose_plan - Выбрать или изменить план\n"
-                       "/info - Получить информацию о планировании\n"
-                       "/help - Показать это сообщение\n "
-                       "/create_plan - Создать план\n "
-                       "/view_plans - Посмотреть план")
                        
 # Обработчик команды для выбора плана
 @dp.message(Command('choose_plan'))
 async def choose_plan_command(message: types.Message):
-    global chech_plan
+    global chech_change_plan, chech_new_plan
+    print("Команда /choose_plan")
     user_id = message.from_user.id
     current_plan = get_user_plan(user_id)
-
+    
     if current_plan:
         await message.reply(f"У вас уже выбран план: {current_plan}. Хотите его изменить? (да/нет)")
+        chech_change_plan = True
     else:
-        for i in plans:
-            await message.reply(plans[i])
-        await message.reply("Выберите вид плана:\n1. Default\n2. Med\n3. Hard\nНапишите 1, 2 или 3.")
-        chech_plan = True
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="Базовый план", callback_data='base_plan')],
+            [InlineKeyboardButton(text="Авторский план", callback_data='current_plan')]
+        ])
 
+        await message.reply("Выберите вид плана:", reply_markup=keyboard)
+
+# Обработчик нажатий на инлайн-кнопки
+@dp.callback_query(lambda c: c.data in ['base_plan', 'current_plan'])
+async def process_plan_selection(callback_query: CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)  # Подтверждаем нажатие кнопки
+
+    if callback_query.data == 'base_plan':
+        await bot.send_message(callback_query.from_user.id, "Вы выбрали Базовый план.")
+        
+        # Получаем все базовые планы
+        base_plans = get_base_plan()  # Получаем планы из базы данных
+        
+        if base_plans:
+            for plan in base_plans:
+                await bot.send_message(callback_query.from_user.id, plan[1])
+                await bot.send_message(callback_query.from_user.id, plan[0])
+
+            # Создаем кнопки для каждого плана
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text=plan[1], callback_data=plan[1]) for plan in base_plans]
+            ])
+            await bot.send_message(callback_query.from_user.id, "Выберите план:", reply_markup=keyboard)
+        else:
+            await bot.send_message(callback_query.from_user.id, "Нет доступных базовых планов.")
+
+    elif callback_query.data == 'current_plan':
+        await bot.send_message(callback_query.from_user.id, "Вы выбрали Актуальный план.")
+        
+        # Получаем все авторские планы
+        user_id = callback_query.from_user.id
+        auth_plans = get_user_plan(user_id)  # Получаем планы из базы данных
+        
+        if auth_plans:
+            for plan in auth_plans:
+                await bot.send_message(callback_query.from_user.id, plan[1])
+                await bot.send_message(callback_query.from_user.id, plan[0])
+
+            # Создаем кнопки для каждого плана
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text=plan[1], callback_data=plan[1]) for plan in auth_plans]
+            ])
+            await bot.send_message(callback_query.from_user.id, "Выберите план:", reply_markup=keyboard)
+        else:
+            await bot.send_message(callback_query.from_user.id, "Нет доступных авторских планов.")
+
+@dp.callback_query(lambda c: c.data in [plan[1] for plan in get_base_plan()])
+async def handle_plan_selection(callback_query: CallbackQuery):
+    selected_plan = callback_query.data
+    name_plan = selected_plan
+    user_id = callback_query.from_user.id
+    update_user_current_plan(user_id,name_plan)
+    await bot.send_message(callback_query.from_user.id, f"Вы выбрали план: {selected_plan}.")
+    # Здесь можно добавить логику для обработки выбранного планаe
+
+# Обработчик выбора плана
+@dp.callback_query(lambda c: c.data in [plan[1] for plan in get_user_plan(c.from_user.id)])
+async def handle_plan_selection(callback_query: CallbackQuery):
+    selected_plan = callback_query.data
+    name_plan = selected_plan
+    user_id = callback_query.from_user.id  # Получаем ID пользователя
+    update_user_current_plan(user_id, name_plan)  # Обновляем текущий план пользователя
+    await bot.send_message(user_id, f"Вы выбрали план: {name_plan}.")  # Отправляем сообщение пользователю
+
+
+# Обработчик команды /info
+@dp.message(Command('info'))
+async def info_command(message: types.Message):
+    print("Команда /info")
+    user_id = message.from_user.id
+    user_name = get_user_name(user_id)
+    
+    if user_name is None:
+        await message.reply('Ваше имя еще не установлено. Пожалуйста, используйте команду /start, чтобы ввести его.')
+    else:
+        await message.reply(f'{user_name}, давай подумаем, а зачем нам вообще нужно планировать? Ведь если подумать — ты каждый день тратишь определённое время на написание плана на день, и появляется вопрос — а зачем это надо? Но мой создатель неплохо так изучил этот вопрос и, главное, проверил планирование в жизни. Если не веришь этому "херу с горы", то послушай 100 роликов (которые он посмотрел) и убедись в этом.\n\n1.  Те 5 минут, которые ты тратишь на составление плана, спасают тебя от суеты посреди дня. Сам подумай, когда тебе легче: когда ты знаешь, что тебе нужно сделать с утра, и, сделав одно, переходишь к другому? Или ты проснулся, думаешь, что тебе надо сделать, сделал это, а потом опять думаешь, и так далее? И при этом поверь мне, ты суетишься, и это отнимает в десятки раз больше времени, чем 5 минут.\n\n2.  Ты можешь думать: Я такая свободная личность, мне бы летать, а я сам себя в клетку сажаю? Нет, тебе так кажется. План как раз освобождает тебя. Ты же знаешь, что "Делу время, а потехе час". Так вот, если ты чётко поставь делу время, то, во-первых, сократишь его, а во-вторых, ты точно будешь знать, когда настанет время для "потехи". В итоге на залипание в стену будет больше времени.\n\n3.  Само дисциплина. Очень полезный навык. Как он работает? Да вот как: когда ты сам написал себе план, будь добр его выполнить. Пусть твоё слово что-то значит. Ты оцениваешь свои возможности, ты ставишь приоритеты, и ты управляешь временем, а не наоборот.На самом деле, очень много плюсов. Если ты хочешь узнать больше, то спроси у "хера с горы", который меня создал.')
+
+@dp.message(Command('check_current_plan'))
+async def check_current_plan_command(message: types.Message):
+    print('команда - check_current_plan')
+    user_id = message.from_user.id
+    current_plan_name = get_current_plan(user_id)
+    print(current_plan_name)
+    if current_plan_name:
+        # Получаем текст плана из base_plans с помощью новой функции. Или если в base_plans нет то в user_plan
+        plan_text = get_plan_text_by_name(current_plan_name)
+        if plan_text:
+            await message.reply(f"Ваш текущий план: {current_plan_name}\n\n{plan_text}")
+        else:
+            await message.reply("План не найден в базе данных.")
+    else:
+        await message.reply("У вас нет текущего плана.")
 
 # Обработчик текстовых сообщений для выбора плана
 @dp.message()
 async def handle_plan_selection(message: types.Message):
-    global chech_name, chech_plan, chech_create_plan
+    global chech_name, chech_new_plan, chech_create_plan, chech_change_plan, chech_base_plan
     user_id = message.from_user.id
     current_plan = get_user_plan(user_id)
     if chech_name:
@@ -96,9 +190,9 @@ async def handle_plan_selection(message: types.Message):
         save_user_name(user_id, user_nick)
         await message.reply(f"Умничка,{user_nick}")
         await message.reply(f'Давай подумаем, а зачем нам вообще нужно планировать? Ведь если подумать — ты каждый день тратишь определённое время на написание плана на день, и появляется вопрос — а зачем это надо? Но мой создатель неплохо так изучил этот вопрос и, главное, проверил планирование в жизни. Если не веришь этому "херу с горы", то послушай 100 роликов (которые он посмотрел) и убедись в этом.\n1.Те 5 минут, которые ты тратишь на составление плана, спасают тебя от суеты посреди дня. Сам подумай, когда тебе легче: когда ты знаешь, что тебе нужно сделать с утра, и, сделав одно, переходишь к другому? Или ты проснулся, думаешь, что тебе надо сделать, сделал это, а потом опять думаешь, и так далее? И при этом поверь мне, ты суетишься, и это отнимает в десятки раз больше времени, чем 5 минут.\n2.Ты можешь думать: Я такая свободная личность, мне бы летать, а я сам себя в клетку сажаю? Нет, тебе так кажется. План как раз освобождает тебя. Ты же знаешь, что "Делу время, а потехе час". Так вот, если ты чётко поставь делу время, то, во-первых, сократишь его, а во-вторых, ты точно будешь знать, когда настанет время для "потехи". В итоге на залипание в стену будет больше времени.\n3.Само дисциплина. Очень полезный навык. Как он работает? Да вот как: когда ты сам написал себе план, будь добр его выполнить. Пусть твоё слово что-то значит. Ты оцениваешь свои возможности, ты ставишь приоритеты, и ты управляешь временем, а не наоборот.На самом деле, очень много плюсов. Если ты хочешь узнать больше, то спроси у "хера с горы", который меня создал.')
-        chech_name =False
+        chech_name = False
 
-    elif chech_plan:
+    elif chech_new_plan:
         if message.text in ['1', '2', '3']:
             plan_types = { '1': 'lite', '2': 'med', '3': 'hard' }
             selected_plan = plan_types[message.text]
@@ -115,24 +209,35 @@ async def handle_plan_selection(message: types.Message):
             await message.reply("Вы оставили текущий план без изменений.")
         else:
             await message.reply("Пожалуйста, выберите 1, 2 или 3 для выбора плана.")
-        chech_plan = False
+        chech_new_plan = False
     elif chech_create_plan:
-        save_user_plan(user_id, message.text)  # Предполагается, что у вас есть функция для сохранения плана
+        test = (message.text).split('\n')
+        name_plan = test[0]
+        text_plan = '\n'.join(test[1:])
+        save_user_plan(user_id,name_plan,text_plan)
         await message.reply(f"Ваш план сохранен: {message.text}.")
         chech_create_plan = False
+    elif chech_change_plan:
+        if message.text.lower() == 'да':
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text="Базовый план", callback_data='base_plan')],
+                [InlineKeyboardButton(text="Актуальный план", callback_data='current_plan')]
+            ])
+            await message.reply("Какой план хотите посмотреть:", reply_markup=keyboard)
+        chech_change_plan = False
+    elif chech_base_plan:
+        id_plan = message.text
+        name_plan = get_plan_name_by_id(id_plan)
+        user_id = message.from_user.id
+        update_user_current_plan(user_id,name_plan)
+        await message.reply(f"Ура. План выбран")
 
-# Обработчик команды /info
-@dp.message(Command('info'))
-async def info_command(message: types.Message):
-    user_id = message.from_user.id
-    user_name = get_user_name(user_id)
-    
-    if user_name is None:
-        await message.reply('Ваше имя еще не установлено. Пожалуйста, используйте команду /start, чтобы ввести его.')
-    else:
-        await message.reply(f'{user_name}, давай подумаем, а зачем нам вообще нужно планировать? Ведь если подумать — ты каждый день тратишь определённое время на написание плана на день, и появляется вопрос — а зачем это надо? Но мой создатель неплохо так изучил этот вопрос и, главное, проверил планирование в жизни. Если не веришь этому "херу с горы", то послушай 100 роликов (которые он посмотрел) и убедись в этом.\n\n1.  Те 5 минут, которые ты тратишь на составление плана, спасают тебя от суеты посреди дня. Сам подумай, когда тебе легче: когда ты знаешь, что тебе нужно сделать с утра, и, сделав одно, переходишь к другому? Или ты проснулся, думаешь, что тебе надо сделать, сделал это, а потом опять думаешь, и так далее? И при этом поверь мне, ты суетишься, и это отнимает в десятки раз больше времени, чем 5 минут.\n\n2.  Ты можешь думать: Я такая свободная личность, мне бы летать, а я сам себя в клетку сажаю? Нет, тебе так кажется. План как раз освобождает тебя. Ты же знаешь, что "Делу время, а потехе час". Так вот, если ты чётко поставь делу время, то, во-первых, сократишь его, а во-вторых, ты точно будешь знать, когда настанет время для "потехи". В итоге на залипание в стену будет больше времени.\n\n3.  Само дисциплина. Очень полезный навык. Как он работает? Да вот как: когда ты сам написал себе план, будь добр его выполнить. Пусть твоё слово что-то значит. Ты оцениваешь свои возможности, ты ставишь приоритеты, и ты управляешь временем, а не наоборот.На самом деле, очень много плюсов. Если ты хочешь узнать больше, то спроси у "хера с горы", который меня создал.')
+        chech_base_plan = False
 
 
+
+def get_text_plan(plan):
+    pass
 
 
 # Функция запуска бота
--- database.py
+++ database.py
@@ -2,25 +2,34 @@ import sqlite3
 
 # Функция для подключения к базе данных
 def get_db_connection():
-    conn = sqlite3.connect('users.db')
+    conn = sqlite3.connect('bd.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 # Функция для создания таблиц
 def create_tables():
-    conn = sqlite3.connect('user_plans.db')
+    """Создает таблицы в базе данных, если они не существуют."""
+    conn = sqlite3.connect('bd.db')
     cursor = conn.cursor()
 
+    # Создаем таблицу user_plans
+    cursor.execute('''CREATE TABLE IF NOT EXISTS user_plans (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        user_name TEXT NOT NULL,
+        plan TEXT NOT NULL)''')
 
+    # Создаем таблицу users
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        user_name TEXT NOT NULL,
+        current_plan TEXT)''')
 
-    # Создаем новую таблицу без ограничения уникальности на user_id
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS user_plans (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER NOT NULL,
-            plan TEXT NOT NULL
-        )
-    ''')
+    # Создаем таблицу base_plans
+    cursor.execute('''CREATE TABLE IF NOT EXISTS base_plans (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name_plan TEXT NOT NULL,
+        text_plan TEXT NOT NULL)''')
 
     conn.commit()
     conn.close()
@@ -29,11 +38,13 @@ def create_tables():
 def get_user_name(user_id):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('SELECT user_name FROM users WHERE user_id = ?', (user_id,))
-    result = cursor.fetchone()
-    conn.close()
-    return result[0] if result else None
-
+    try:
+        cursor.execute('SELECT user_name FROM users WHERE user_id = ?', (user_id,))
+        result = cursor.fetchone()
+        conn.close()
+        return result[0] if result else None
+    except sqlite3.Error as e:
+        return None
 # Функция для сохранения имени пользователя в базе данных
 def save_user_name(user_id, user_name):
     conn = get_db_connection()
@@ -45,11 +56,11 @@ def save_user_name(user_id, user_name):
 # Функция для получения плана пользователя
 def get_user_plan(user_id):
     # Подключаемся к базе данных
-    conn = sqlite3.connect('user_plans.db')
+    conn = sqlite3.connect('bd.db')
     cursor = conn.cursor()
 
     # Извлекаем план пользователя
-    cursor.execute('SELECT plan FROM user_plans WHERE user_id = ?', (user_id,))
+    cursor.execute('SELECT plan, name_plan FROM user_plans WHERE user_id = ?', (user_id,))
     result = cursor.fetchall()
     conn.close()
 
@@ -57,15 +68,68 @@ def get_user_plan(user_id):
         return result  # Возвращаем текст планов
     return None  # Если плана нет, возвращаем None
 
-# Функция для сохранения плана пользователя
-def save_user_plan(user_id, plan_text):
-    # Подключаемся к базе данных
-    conn = sqlite3.connect('user_plans.db')
+def get_base_plan():
+    conn = sqlite3.connect('bd.db')
     cursor = conn.cursor()
 
-    # Сохраняем новый план в таблице
-    cursor.execute('''INSERT INTO user_plans (user_id, plan) VALUES (?, ?)''', (user_id, plan_text))
+    # Извлекаем план пользователя
+    cursor.execute('SELECT text_plan, name_plan FROM base_plans ')
+    result = cursor.fetchall()
+    conn.close()
+    if result:
+        return result  # Возвращаем текст планов
+    return None  # Если плана нет, возвращаем None
 
-    # Сохраняем изменения и закрываем соединение
+# Функция для сохранения плана пользователя
+def save_user_plan(user_id,name_plan, plan_text):
+    try:
+        conn = sqlite3.connect('bd.db')
+        cursor = conn.cursor()
+        user_name = get_user_name(user_id)
+        # Сохраняем новый план в таблице
+        cursor.execute('''
+            INSERT INTO user_plans (user_id, user_name, name_plan, plan) VALUES (?, ?, ?, ?)
+        ''', (user_id, user_name, name_plan, plan_text))
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Ошибка при сохранении плана: {e}")
+    finally:
+        conn.close()
+
+# Функция для получения name_plan по id
+def get_plan_name_by_id(plan_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT name_plan FROM base_plans WHERE id = ?', (plan_id,))
+    result = cursor.fetchone()
+    conn.close()
+    return result[0] if result else None
+
+# Функция для обновления current_plan в таблице users
+def update_user_current_plan(user_id, plan_name):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('UPDATE users SET current_user_plan = ? WHERE user_id = ?', (plan_name, user_id))
     conn.commit()
     conn.close()
+
+# Функция для получения текущего плана пользователя
+def get_current_plan(user_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT current_user_plan FROM users WHERE user_id = ?', (user_id,))
+    result = cursor.fetchone()
+    conn.close()
+    return result[0] if result else None
+
+def get_plan_text_by_name(plan_name):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT text_plan FROM base_plans WHERE name_plan = ?', (plan_name,))
+    result = cursor.fetchone()
+    if not result:
+        cursor.execute('SELECT plan FROM user_plans WHERE name_plan = ?', (plan_name,))
+        result = cursor.fetchone()
+    conn.close()
+    return result[0] if result else None
+

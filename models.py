a7f34f12293e4fded89f7e219b2f5a87ee708638
Nikolay Savchenko <nikolay.savchenko@redcollar.ru>
2025-05-16 15:26:24 +0300

    refactor

--- models.py
+++ models.py
@@ -92,7 +92,7 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def init_db():
-    Base.metadata.drop_all(bind=engine)
+    # Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
 def get_db():
